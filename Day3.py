1137a038f0aef4529c37d7be3a6114ee26bee7d6
Shamaah Logbi <candle1416@hotmail.com>
2019-08-22 21:34:27 +0300

    Day3

---
+++ Day3.py
@@ -0,0 +1,31 @@
+x=5 # x is type of int
+x="Sally" #x is now type of string
+print(x)
+print("-----")
+#double quotes same as single quotes
+y="John"
+print(y)
+y='John'
+print(y)
+print("-----")
+#assign values to multiple variables in one line
+x,y,z="Orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+print("-----")
+#assign value to multiple variables in one line
+x=y=z="Fruits"
+print(x)
+print(y)
+print(z)
+print("-----")
+#Combine both text and a variable
+x="awesome"
+print("Python is " + x)
+#add a variable to another variable
+x,y="Python is ","awesome"
+print(x+y)
+#addition
+x,y=5,10
+print(x+y)
