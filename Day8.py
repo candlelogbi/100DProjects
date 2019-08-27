bfe7ad176b39b01d679d755a008d50df3f566f09
Shamaah Logbi <candle1416@hotmail.com>
2019-08-27 22:41:30 +0300

    Day 8

---
+++ Day8.py
@@ -0,0 +1,11 @@
+a="Hello World"
+#remove whitespace at the end (r,l)
+print(a.strip())
+#length of string
+print(len(a))
+print(a.lower())
+print(a.upper())
+print(a.replace("H","J"))
+print(a.split(" "))
+
+
