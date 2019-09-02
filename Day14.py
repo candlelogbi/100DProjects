f9db3c98fbf9e4e9c657927ab219fbc05ab65d37
Shamaah Logbi <candle1416@hotmail.com>
2019-09-03 00:11:25 +0300

    Day 14

---
+++ Day14.py
@@ -0,0 +1,12 @@
+color=["bule","yellow","green","red"]
+print(color[1:2])
+if "red" in color:
+ print("red is item of color list")
+
+# repeat an item *
+rNumbers=[4]*6
+print(rNumbers)
+fNumber=[1.4,7.8,5.3]
+
+newList=fNumber+rNumbers
+print(newList)
