f4a38fa7c1b12146309902348b223d1106f15ade
Shamaah Logbi <candle1416@hotmail.com>
2019-09-12 21:39:30 +0300

    Day 25

---
+++ Day25.py
@@ -0,0 +1,15 @@
+#1
+myset={1,3,5,7,8}
+myset.update([4,8,12])
+myset.remove(8)
+myset.clear()
+print(myset)
+#2
+mydict={
+"name":"pigeon",
+"type":"bird",
+"skin cover":"feathers"
+}
+print(mydict["type"])
+mydict["skin cover"]="fur"
+print(mydict)
