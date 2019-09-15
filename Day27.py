c35b2bd7ee6dd6ec2bd21c57946b229862023a70
Shamaah Logbi <candle1416@hotmail.com>
2019-09-15 22:36:50 +0300

    Day 27

---
+++ Day27.py
@@ -0,0 +1,22 @@
+a ,b=5,3
+if a > b: 
+    print("a greater than b")
+elif a == b:
+    print("a greater than b")
+else:
+ print("b greater than a")
+score=79
+print("Pass") if score >= 60 else print("Fail")
+print("A") if score >= 90 else print("B") if score==80 else print("C")
+x=40
+if x<50 and x>30: print("a between 50-30")
+if x>50 or x<30: print("a between 50-30")
+age,gender=3,"Female"
+if age >= 4: 
+  if gender=="Female":
+    print("KG2 Girls section")
+  else:
+    print("KG2 Boys section")
+
+else:
+ print("KG1")
