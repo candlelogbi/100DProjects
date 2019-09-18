ebe70b27b4dfeaf88c74c3e1265c6dd9722557fb
Shamaah Logbi <candle1416@hotmail.com>
2019-09-18 21:46:14 +0300

    Day 30

---
+++ Day30.py
@@ -0,0 +1,14 @@
+#range(start,end,increment)
+#end-1 last nummber
+for  x in range(5):
+   print(x)
+else:   
+ print("Finally finish!")   
+for  x in range(1,7,2):
+   print(x) 
+
+fruits=["mango","apple"]
+color=["yellow","red","orange"]
+for  x in color:
+    for y in fruits:
+       print(x,y)
