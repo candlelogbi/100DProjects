bb459ef93d69047ca7be3f42dadfa6f59b6a4315
Shamaah Logbi <candle1416@hotmail.com>
2019-09-19 20:50:03 +0300

    Day 31

---
+++ Day31.py
@@ -0,0 +1,12 @@
+# def fun_name(parameters):
+        #rest
+def myFunction():
+    print("First Function")
+myFunction()            
+
+def myFunction2(name="Khaled"):
+    print("Grade 1:"+ name)
+myFunction2("Ahmed")    
+myFunction2("Maha")    
+myFunction2("Yahya")    
+myFunction2()
