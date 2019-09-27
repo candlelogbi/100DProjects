f9a9bf2291a686351385c2bf1612060a57d49a9a
Shamaah Logbi <candle1416@hotmail.com>
2019-09-27 21:02:00 +0300

    Day 38

---
+++ Day38.py
@@ -0,0 +1,14 @@
+array=[1,3,4,6]
+for x in array:
+  print(x)
+array.append(12)
+array.append(6)
+print(array)
+array.pop(2) 
+print(array)
+num=array.count(6)
+print(num)
+#delete first occurrence  value
+array.remove(6)
+print(array)
+
